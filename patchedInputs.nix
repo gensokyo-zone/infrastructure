1532d7c1830a82d44e3feae7c7a148dfddf99b96
arcnmx <git@git.arcn.mx>
2024-01-18 13:10:23 -0800

    chore: patch nixos k8s bug

--- patchedInputs.nix
+++ patchedInputs.nix
@@ -6,21 +6,16 @@
   pkgs = import ./overlays {inherit inputs system;}; # A local import of nixpkgs without patching.
 in
   inputs
-/*
           // {
   nixpkgs = pkgs.applyPatches {
     name = "nixpkgs";
     src = inputs.nixpkgs;
     patches = [
+      # https://github.com/NixOS/nixpkgs/pull/275896
+      (pkgs.fetchpatch {
+        url = "https://github.com/NixOS/nixpkgs/pull/275896.patch";
+        sha256 = "sha256-boJLCdgamzX0fhLifdsxsFF/f7oXZwWJ7+WAkcA2GBg=";
+      })
     ];
   };
-} // { darwin = pkgs.applyPatches {
-  name = "darwin";
-  src = inputs.darwin;
-  patches = [ (pkgs.fetchpatch {
-    url = "https://patch-diff.githubusercontent.com/raw/LnL7/nix-darwin/pull/310.patch";
-    sha256 = "sha256-drnLOhF8JGXx8YY7w1PD2arUZvbqafWPTatQNTHt+QI=";
-  }) ];
-}; }
-*/
-
+}
