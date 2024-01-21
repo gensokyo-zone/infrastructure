2a1c9513a03a7bc8123c5bedcfe9163c8e7ed45d
Kat Inskip <kat@inskip.me>
2024-01-20 16:13:39 -0800

    ops(k8s): init metallb, move cloudflare-operator to follow same layout

--- k8s/bootstrap/bootstrap.sh
+++ k8s/bootstrap/bootstrap.sh
@@ -10,11 +10,11 @@ helm --namespace=kube-system install coredns coredns/coredns --set service.clust
 echo "Installing ArgoCD (GitOps)"
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd --namespace argocd --create-namespace
-echo "Generating an age keyfile for sops-secrets-operator, the next line will be the public key, please add it to .sops.yaml"
-rage-keygen -o keys.txt 2>&1 | awk '{ print $3 }'
+echo "Installing sops-secrets-operator"
+#echo "Generating an age keyfile for sops-secrets-operator, the next line will be the public key, please add it to .sops.yaml"
+#rage-keygen -o keys.txt 2>&1 | awk '{ print $3 }'
 kubectl create ns sops
 kubectl create secret generic sops-age-key-file --from-file=keys.txt -o yaml --dry-run=client -n sops > sops-age-key-file.yaml
 kubectl apply -f sops-age-key-file.yaml -n sops
-echo "Installing sops-secrets-operator"
 helm repo add sops-secrets-operator https://inloco.github.io/sops-secrets-operator
 helm install sops-secrets-operator sops-secrets-operator/sops-secrets-operator --namespace sops --values sops-values.yaml
