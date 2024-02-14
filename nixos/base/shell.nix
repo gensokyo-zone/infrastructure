a26b0fb33766a1f71951c34fca75bd7a59a89dac
arcnmx <git@git.arcn.mx>
2024-02-13 16:57:19 -0800

    chore: shell histignore defaults

--- nixos/base/shell.nix
+++ nixos/base/shell.nix
@@ -4,5 +4,16 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    interactiveShellInit = ''
+      setopt autocd
+      setopt HIST_IGNORE_DUPS
+      setopt HIST_IGNORE_SPACE
+      HISTORY_IGNORE="(*^C*|:*|/*|~*|.*)"
+    '';
+  };
+  programs.bash = {
+    interactiveShellInit = ''
+      HISTIGNORE="[bf]g:exit: *:*^C*"
+    '';
   };
 }
