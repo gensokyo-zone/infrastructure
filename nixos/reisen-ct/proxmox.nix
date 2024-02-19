ee2618061d61e6650152a11cbd78fdcaaa94aac2
arcnmx <git@git.arcn.mx>
2024-02-19 09:08:42 -0800

    chore: lean closures

--- nixos/reisen-ct/proxmox.nix
+++ nixos/reisen-ct/proxmox.nix
@@ -1,10 +1,19 @@
 {
+  lib,
   modulesPath,
   ...
-}: {
+}: let
+  inherit (lib.modules) mkDefault;
+in {
   imports = [
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
   ];
 
-  services.getty.autologinUser = "root";
+  services.getty.autologinUser = mkDefault "root";
+  documentation.enable = mkDefault false;
+
+  environment.variables = {
+    # nix default is way too big
+    GC_INITIAL_HEAP_SIZE = mkDefault "8M";
+  };
 }
