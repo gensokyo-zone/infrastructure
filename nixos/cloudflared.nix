565e51842773116109bec435099212504f868d06
arcnmx <git@git.arcn.mx>
2024-03-01 15:26:15 -0800

    fix(cloudflared): udp buffer sizes

--- nixos/cloudflared.nix
+++ nixos/cloudflared.nix
@@ -3,7 +3,15 @@
   lib,
   ...
 }: let
-  inherit (lib.modules) mkDefault;
+  inherit (lib.modules) mkIf mkDefault;
+  cfg = config.services.cloudflared;
 in {
-  config.services.cloudflared.enable = mkDefault true;
+  config = {
+    services.cloudflared.enable = mkDefault true;
+    boot.kernel.sysctl = mkIf (!config.boot.isContainer && cfg.enable) {
+      # https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
+      "net.core.rmem_max" = mkDefault 2500000;
+      "net.core.wmem_max" = mkDefault 2500000;
+    };
+  };
 }
