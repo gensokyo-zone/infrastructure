7d6a31b37b16c2a9de91266fb3769ce443472cbc
arcnmx <git@git.arcn.mx>
2024-03-16 13:35:17 -0700

    fix: nixos local hostname resolution

--- nixos/base/network.nix
+++ nixos/base/network.nix
@@ -1,10 +1,11 @@
 {
   inputs,
   name,
+  config,
   lib,
   ...
 }: let
-  inherit (lib.modules) mkDefault mkOverride;
+  inherit (lib.modules) mkIf mkOrder mkDefault mkOverride;
   inherit (inputs.self.lib.lib) domain;
 in {
   networking = {
@@ -12,4 +13,9 @@ in {
     domain = mkDefault domain;
     hostName = mkOverride 25 name;
   };
+
+  # work around https://github.com/NixOS/nixpkgs/issues/132646
+  system.nssDatabases.hosts = mkIf config.services.resolved.enable (
+    mkOrder 500 [ "files" ]
+  );
 }
