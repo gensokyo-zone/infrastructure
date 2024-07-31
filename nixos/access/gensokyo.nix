9e1a9aa752ecb135abb1e3f7af31b0ab9f6e5a2a
arcnmx <git@git.arcn.mx>
2024-07-31 11:59:51 -0700

    feat(nginx): OIDC webfinger

--- nixos/access/gensokyo.nix
+++ nixos/access/gensokyo.nix
@@ -4,9 +4,13 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  inherit (lib.modules) mkMerge mkAfter;
+  inherit (lib.strings) escapeRegex;
+  inherit (gensokyo-zone.lib) domain;
+in {
   services.nginx.virtualHosts.gensokyoZone = {
-    serverName = config.networking.domain;
+    serverName = domain;
     locations = {
       "/" = {
         root = gensokyo-zone.inputs.website.packages.${pkgs.system}.gensokyoZone;
@@ -17,6 +21,37 @@
             name = "docs";
             path = gensokyo-zone.self.packages.${pkgs.system}.docs;
           }
+        ];
+      };
+      "/.well-known/webfinger" = let
+        # https://www.rfc-editor.org/rfc/rfc7033#section-3.1
+        oidc = {
+          subject = "acct:${acct}@${domain}";
+          links = [
+            {
+              rel = "http://openid.net/specs/connect/1.0/issuer";
+              href = "https://sso.${domain}/realms/${domain}";
+            }
+          ];
+        };
+        acct = "$webfinger_oidc_acct";
+      in {
+        headers.set.Access-Control-Allow-Origin = "*";
+        extraConfig = mkMerge [
+          ''
+            set ${acct} "";
+            if ($arg_resource ~* "^acct(%3A|:)([^%@]*)(%40|@)${escapeRegex domain}$") {
+              set ${acct} $2;
+              add_header "Content-Type" "application/jrd+json";
+            }
+            if ($arg_rel !~* "http.*openid\.net") {
+              set ${acct} "";
+            }
+            if (${acct} = "") {
+              return 404;
+            }
+          ''
+          (mkAfter "return 200 '${builtins.toJSON oidc}';")
         ];
       };
     };
