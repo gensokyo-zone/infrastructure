b7f5cd0562da6fbe8b8664e1730a478874203901
Kat Inskip <kat@inskip.me>
2024-09-02 22:28:39 -0700

    feat(litterbox): change path to mount

--- nixos/syncthing-kat/syncthing.nix
+++ nixos/syncthing-kat/syncthing.nix
@@ -3,6 +3,7 @@
     enable = true;
     guiAddress = "0.0.0.0:8384";
     openDefaultPorts = true;
+    dataDir = "/mnt/kyuuto-litterbox";
   };
   networking.firewall.interfaces.local.allowedTCPPorts = [ 8384 ];
 }
