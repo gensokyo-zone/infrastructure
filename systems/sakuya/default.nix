c2089adbc7590aee4d87f4631886a15401b6dbd9
Kat Inskip <kat@inskip.me>
2024-09-05 00:57:19 -0700

    feat(sakuya): new machine!

---
+++ systems/sakuya/default.nix
@@ -0,0 +1,27 @@
+_: {
+  arch = "aarch64";
+  type = "NixOS";
+  ci.allowFailure = true;
+  access.online.enable = false;
+  modules = [
+    ./nixos.nix
+  ];
+deploy = {
+hostname = "10.1.1.50";
+};
+  network.networks = {
+    tail = {
+      #address4 = "100.70.124.79";
+      #address6 = "fd7a:115c:a1e0::b001:7c4f";
+    };
+    local = {
+      macAddress = "02:ba:46:f8:40:52";
+      address4 = "10.1.1.50";
+    };
+  };
+  exports = {
+    services = {
+      tailscale.enable = true;
+    };
+  };
+}
---
+++ systems/sakuya/nixos.nix
@@ -0,0 +1,46 @@
+{ config, meta, ... }: {
+
+  imports = let
+    inherit (meta) nixos;
+   in [
+    nixos.sops
+    nixos.tailscale
+  ];
+  boot.loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
+
+  fileSystems."/" = {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
+
+  networking.useNetworkd = true;
+  systemd.network = {
+    networks."40-end0" = {
+      inherit (config.systemd.network.links.end0) matchConfig;
+      address = ["10.1.1.50/24"];
+      gateway = ["10.1.1.1"];
+      DHCP = "no";
+      networkConfig = {
+        IPv6AcceptRA = true;
+      };
+      linkConfig = {
+        Multicast = true;
+      };
+    };
+    links.end0 = {
+      matchConfig = {
+        Type = "ether";
+        MACAddress = "02:ba:46:f8:40:52";
+      };
+      linkConfig = {
+        WakeOnLan = "magic";
+      };
+    };
+  };
+  sops.defaultSopsFile = ./secrets.yaml;
+
+  system.stateVersion = "24.11";
+}
