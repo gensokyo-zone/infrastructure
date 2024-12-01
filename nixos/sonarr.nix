e27ecbb13a86f04404afe0e41dc218e9ec7c4424
arcnmx <git@git.arcn.mx>
2024-11-30 18:53:07 -0800

    fix(sonarr): disable if insecure

--- nixos/sonarr.nix
+++ nixos/sonarr.nix
@@ -1,6 +1,10 @@
-_: {
+{config, lib, ...}: let
+  inherit (lib.modules) mkIf mkDefault;
+  cfg = config.services.sonarr;
+  available = cfg.package.meta.available && cfg.package.dotnet-runtime.meta.available or true;
+in {
   services.sonarr = {
-    enable = true;
+    enable = mkIf available (mkDefault true);
   };
   users.users.sonarr.extraGroups = ["kyuuto"];
 }
