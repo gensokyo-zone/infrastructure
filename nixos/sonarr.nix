ea870818a92aff74bde0169b94495fc88fb41fa9
arcnmx <git@git.arcn.mx>
2024-12-13 19:22:12 -0800

    fix(sonarr): outdated .NET check

--- nixos/sonarr.nix
+++ nixos/sonarr.nix
@@ -1,7 +1,7 @@
 {config, lib, ...}: let
   inherit (lib.modules) mkIf mkDefault;
   cfg = config.services.sonarr;
-  available = cfg.package.meta.available && cfg.package.dotnet-runtime.meta.available or true;
+  available = cfg.package.meta.available && cfg.package.dotnet-runtime.meta.available or true && lib.versionAtLeast cfg.package.dotnet-runtime.version or "8.0" "8.0";
 in {
   services.sonarr = {
     enable = mkIf available (mkDefault true);
