214007612f0577e3baf03c2b8a163c0091dfada0
arcnmx <git@git.arcn.mx>
2025-01-02 22:13:26 -0800

    fix(mediatomb): broken and out of date package

--- nixos/mediatomb.nix
+++ nixos/mediatomb.nix
@@ -5,9 +5,10 @@
 }: let
   inherit (lib.modules) mkDefault mkIf;
   cfg = config.services.mediatomb;
+  gerberaUpdated = lib.versionAtLeast config.services.mediatomb.package.version "1.12.2";
 in {
   config.services.mediatomb = {
-    enable = mkDefault true;
+    enable = mkIf gerberaUpdated (mkDefault true);
     port = mkDefault 4152;
     uuid = mkDefault "082fd344-bf69-5b72-a68f-a5a4d88e76b2";
   };
