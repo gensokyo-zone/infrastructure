bc8ed413d0dc2213d93618a7ba1338d609cf0e23
Kat Inskip <kat@inskip.me>
2025-09-20 14:12:13 -0700

    feat: litterbox 2, remove users

---
+++ systems/litterbox-2/default.nix
@@ -0,0 +1,23 @@
+_: {
+  imports = [
+    ./proxmox.nix
+  ];
+  arch = "x86_64";
+  type = "NixOS";
+  ci.allowFailure = true;
+  access.online.enable = false;
+  modules = [
+    ./nixos.nix
+  ];
+  network.networks = {
+    tail = {
+      address4 = "100.70.124.79";
+      address6 = "fd7a:115c:a1e0::b001:7c4f";
+    };
+  };
+  exports = {
+    services = {
+      tailscale.enable = true;
+    };
+  };
+}
