318bb7185c8d0d7849f599e7512019c828b97ac0
Kat Inskip <kat@inskip.me>
2025-11-15 21:17:20 -0800

    fix: just add random addresses

--- systems/mail/default.nix
+++ systems/mail/default.nix
@@ -10,15 +10,15 @@ _: {
   ];
   network.networks = {
     tail = {
-      #address4 = "100.78.97.73";
-      #address6 = "fd7a:115c:a1e0::d834:6149";
+      address4 = "100.78.97.73";
+      address6 = "fd7a:115c:a1e0::d834:6149";
     };
   };
   exports = {
     services = {
       promtail.enable = false;
       prometheus-exporters-node.enable = false;
-      tailscale.enable = false;
+      tailscale.enable = true;
     };
   };
 }
